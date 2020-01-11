6da98c35140ac0bfa63d66c1066a173b3a06354a
Ruben Segura <Ruben@MacBook-Pro-de-Ruben.local>
2020-01-11 12:00:37 +0100

    add React app

---
+++ precache-manifest.802eed05638fa3f3ed2b3f2945cbb66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147fd117883d4610671d5edcec5234bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ee37cd827b00af4d37",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "427bb7474a9f6ee4af94",
+    "url": "/static/css/main.c79b04f2.chunk.css"
+  },
+  {
+    "revision": "42ee37cd827b00af4d37",
+    "url": "/static/js/2.25410563.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.25410563.chunk.js.LICENSE"
+  },
+  {
+    "revision": "427bb7474a9f6ee4af94",
+    "url": "/static/js/main.d2266ff6.chunk.js"
+  },
+  {
+    "revision": "050f3fabef2ea9c58be4",
+    "url": "/static/js/runtime-main.e7dd45ef.js"
+  }
+]);
